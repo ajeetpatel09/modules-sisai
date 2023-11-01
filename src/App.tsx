c7dfb6d6490365823c2b2cb153ff0b9592d1535d
Ajeet Patel <ajeet.patel@scrobits.com>
2023-11-01 16:33:57 +0530

    added zingchart

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,11 @@ function App() {
   return (
     <>
       <NavLink to='/' style={{color: 'inherit'}}><h3>1. Authentication</h3></NavLink>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>2. Web socket</h3></NavLink>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>3. Cron jobs- send mails</h3></NavLink>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>4. Cron jobs- update device status</h3></NavLink>
-      <NavLink to='/createExcel' style={{color: 'inherit'}}><h3>5. Excel sheet creation from backend and frontend</h3></NavLink>
-      <NavLink to='/pdfCreation' style={{color: 'inherit'}}><h3>6. pdf creation at frontend</h3></NavLink>
-      <NavLink to='/chart' style={{color: 'inherit'}}><h3>7. Chart rendering</h3></NavLink>
-      <NavLink to='/Geocoding' style={{color: 'inherit'}}><h3>8. Geocoding</h3></NavLink>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>9. Link based reset/create password</h3></NavLink>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>10. MQTT connection</h3></NavLink>
-      <NavLink to='/maps' style={{color: 'inherit'}}><h3>11. Maps rendering</h3></NavLink>
+      <NavLink to='/createExcel' style={{color: 'inherit'}}><h3>2. Excel sheet creation from backend and frontend</h3></NavLink>
+      <NavLink to='/pdfCreation' style={{color: 'inherit'}}><h3>3. pdf creation at frontend</h3></NavLink>
+      <NavLink to='/chart' style={{color: 'inherit'}}><h3>4. Chart rendering</h3></NavLink>
+      <NavLink to='/Geolocation' style={{color: 'inherit'}}><h3>5. Get current location</h3></NavLink>
+      <NavLink to='/maps' style={{color: 'inherit'}}><h3>6. Maps rendering</h3></NavLink>
     </>
   )
 }
--- src/routes/RoutingConfig.tsx
+++ src/routes/RoutingConfig.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         element: <ExcelSheet/>
     },
     {
-        path: '/Geocoding',
+        path: '/Geolocation',
         element: <GeoLocation/>
     },
     {
