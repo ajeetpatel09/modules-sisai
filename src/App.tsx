546046919f209e0cf01ef6063647efd20f44bda5
Ajeet Patel <ajeet.patel@scrobits.com>
2023-11-01 16:38:29 +0530

    few modifications

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ function App() {
 
   return (
     <>
-      <NavLink to='/' style={{color: 'inherit'}}><h3>1. Authentication</h3></NavLink>
-      <NavLink to='/createExcel' style={{color: 'inherit'}}><h3>2. Excel sheet creation from backend and frontend</h3></NavLink>
-      <NavLink to='/pdfCreation' style={{color: 'inherit'}}><h3>3. pdf creation at frontend</h3></NavLink>
-      <NavLink to='/chart' style={{color: 'inherit'}}><h3>4. Chart rendering</h3></NavLink>
-      <NavLink to='/Geolocation' style={{color: 'inherit'}}><h3>5. Get current location</h3></NavLink>
-      <NavLink to='/maps' style={{color: 'inherit'}}><h3>6. Maps rendering</h3></NavLink>
+      <NavLink to='/createExcel' style={{color: 'inherit'}}><h3>1. Excel sheet creation</h3></NavLink>
+      <NavLink to='/pdfCreation' style={{color: 'inherit'}}><h3>2. pdf creation</h3></NavLink>
+      <NavLink to='/chart' style={{color: 'inherit'}}><h3>3. Chart rendering</h3></NavLink>
+      <NavLink to='/Geolocation' style={{color: 'inherit'}}><h3>4. Get current location</h3></NavLink>
+      <NavLink to='/maps' style={{color: 'inherit'}}><h3>5. Maps rendering</h3></NavLink>
     </>
   )
 }
